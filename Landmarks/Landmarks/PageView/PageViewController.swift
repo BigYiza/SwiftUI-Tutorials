58960487c8a886be9ef802894d290b5adb505d89
BigYiza <bear.zz@qq.com>
2024-05-24 16:17:35 +0800

    5_Landmarks Demo：UIKit

--- Landmarks/Landmarks/PageView/PageViewController.swift
+++ Landmarks/Landmarks/PageView/PageViewController.swift
@@ -5,4 +5,24 @@
 //  Created by 张义 on 2024/5/23.
 //
 
-import Foundation
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+        
+    var pages: [Page]
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+            let pageViewController = UIPageViewController(
+                transitionStyle: .scroll,
+                navigationOrientation: .horizontal)
+
+
+            return pageViewController
+        }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+            pageViewController.setViewControllers(
+                [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+        }
+}
